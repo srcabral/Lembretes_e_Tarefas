0580004769ac547c932fdb0af61bc0f32c617fa0
Sergio_Cabral <scastrocabral@gmail.com>
2022-01-06 23:09:06 -0300

    Treats button Cancel and New Task. Add time on the textinputedit of hour

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -3,9 +3,13 @@ package com.example.todolist.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.datasource.TaskDataSource
 import com.example.todolist.extensions.format
 import com.example.todolist.extensions.text
+import com.example.todolist.model.Task
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 class AddTaskActivity : AppCompatActivity() {
@@ -29,6 +33,33 @@ class AddTaskActivity : AppCompatActivity() {
                 binding.tilDataCreateTask.text =  Date(it + offset).format()
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+
+        binding.tilHoursCreateTask.editText?.setOnClickListener{
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+            timePicker.addOnPositiveButtonClickListener {
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+
+                binding.tilHoursCreateTask.text = "$hour : $minute"
+            }
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+
+        binding.btnNewTask.setOnClickListener {
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilDataCreateTask.text,
+                hour = binding.tilHoursCreateTask.text
+            )
+            TaskDataSource.insertTask(task)
         }
     }
 }
